8e4e06a313fbe3ebfcac4068663e46b505a4f0aa
Luke Gaunt <lagaunt120@yahoo.com>
2018-02-14 20:41:31 +0000

    jQuery reversion back to JavaScript

---
+++ external/js.js
@@ -0,0 +1,231 @@
+//code for first question
+
+function show1() {
+
+    var answer = document.querySelector('#a1');
+    
+    answer.style.visibility = "visible";
+  
+}
+  
+function hide1() {
+    
+    var answer = document.querySelector('#a1');
+    
+    answer.style.visibility = "hidden";
+  
+}
+
+//code for second question
+
+function show2() {
+
+    var answer = document.querySelector('#a2');
+    
+    answer.style.visibility = "visible";
+  
+}
+  
+function hide2() {
+    
+    var answer = document.querySelector('#a2');
+    
+    answer.style.visibility = "hidden";
+  
+}
+
+//code for third question
+
+function show3() {
+
+    var answer = document.querySelector('#a3');
+    
+    answer.style.visibility = "visible";
+  
+}
+  
+function hide3() {
+    
+    var answer = document.querySelector('#a3');
+    
+    answer.style.visibility = "hidden";
+  
+}
+
+//code for fourth question
+
+function show4() {
+
+    var answer = document.querySelector('#a4');
+    
+    answer.style.visibility = "visible";
+  
+}
+  
+function hide4() {
+    
+    var answer = document.querySelector('#a4');
+    
+    answer.style.visibility = "hidden";
+  
+}
+
+//code for fifth question
+
+function show5() {
+
+    var answer = document.querySelector('#a5');
+    
+    answer.style.visibility = "visible";
+  
+}
+  
+function hide5() {
+    
+    var answer = document.querySelector('#a5');
+    
+    answer.style.visibility = "hidden";
+  
+}
+
+//code for sixth question
+
+function show6() {
+
+    var answer = document.querySelector('#a6');
+    
+    answer.style.visibility = "visible";
+  
+}
+  
+function hide6() {
+    
+    var answer = document.querySelector('#a6');
+    
+    answer.style.visibility = "hidden";
+  
+}
+
+//code for seventh question
+
+function show7() {
+
+    var answer = document.querySelector('#a7');
+    
+    answer.style.visibility = "visible";
+  
+}
+  
+function hide7() {
+    
+    var answer = document.querySelector('#a7');
+    
+    answer.style.visibility = "hidden";
+  
+}
+
+//code for eighth question
+
+function show8() {
+
+    var answer = document.querySelector('#a8');
+    
+    answer.style.visibility = "visible";
+  
+}
+  
+function hide8() {
+    
+    var answer = document.querySelector('#a8');
+    
+    answer.style.visibility = "hidden";
+  
+}
+
+//code for ninth question
+
+function show9() {
+
+    var answer = document.querySelector('#a9');
+    
+    answer.style.visibility = "visible";
+  
+}
+  
+function hide9() {
+    
+    var answer = document.querySelector('#a9');
+    
+    answer.style.visibility = "hidden";
+  
+}
+
+//code for tenth question
+
+function show10() {
+
+    var answer = document.querySelector('#a10');
+    
+    answer.style.visibility = "visible";
+  
+}
+  
+function hide10() {
+    
+    var answer = document.querySelector('#a10');
+    
+    answer.style.visibility = "hidden";
+  
+}
+
+//code for "Show All" and "Hide All" buttons
+
+function showAll() {
+    
+    var answer1 = document.querySelector('#a1');
+    var answer2 = document.querySelector('#a2');
+    var answer3 = document.querySelector('#a3');
+    var answer4 = document.querySelector('#a4');
+    var answer5 = document.querySelector('#a5');
+    var answer6 = document.querySelector('#a6');
+    var answer7 = document.querySelector('#a7');
+    var answer8 = document.querySelector('#a8');
+    var answer9 = document.querySelector('#a9');
+    var answer10 = document.querySelector('#a10');
+
+    answer1.style.visibility = "visible";
+    answer2.style.visibility = "visible";
+    answer3.style.visibility = "visible";
+    answer4.style.visibility = "visible";
+    answer5.style.visibility = "visible";
+    answer6.style.visibility = "visible";
+    answer7.style.visibility = "visible";
+    answer8.style.visibility = "visible";
+    answer9.style.visibility = "visible";
+    answer10.style.visibility = "visible";
+}
+
+function hideAll() {
+    
+    var answer1 = document.querySelector('#a1');
+    var answer2 = document.querySelector('#a2');
+    var answer3 = document.querySelector('#a3');
+    var answer4 = document.querySelector('#a4');
+    var answer5 = document.querySelector('#a5');
+    var answer6 = document.querySelector('#a6');
+    var answer7 = document.querySelector('#a7');
+    var answer8 = document.querySelector('#a8');
+    var answer9 = document.querySelector('#a9');
+    var answer10 = document.querySelector('#a10');
+
+    answer1.style.visibility = "hidden";
+    answer2.style.visibility = "hidden";
+    answer3.style.visibility = "hidden";
+    answer4.style.visibility = "hidden";
+    answer5.style.visibility = "hidden";
+    answer6.style.visibility = "hidden";
+    answer7.style.visibility = "hidden";
+    answer8.style.visibility = "hidden";
+    answer9.style.visibility = "hidden";
+    answer10.style.visibility = "hidden";
+}
